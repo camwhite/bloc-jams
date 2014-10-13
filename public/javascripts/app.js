58a8123e2956aaffdb9ea167ceca0c6141083d9f
Cameron White <camw85@mac.com>
2014-10-13 20:29:31 +0000

    Functional seekbars

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -208,14 +208,58 @@ var changeAlbumView = function(album) {
 };
 
 
-// This 'if' condition is used to prevent the jQuery modifications
-// from happening on non-Album view pages.
-//  - Use a regex to validate that the url has "/album" in its path.
-if (document.URL.match(/\/album.html/)) {
- // Wait until the HTML is fully processed.
+var updateSeekPercentage = function($seekBar, event) {
+ var barWidth = $seekBar.width();
+ var offsetX = event.pageX - $seekBar.offset().left;
+
+ var offsetXPercent = (offsetX  / barWidth) * 100;
+ offsetXPercent = Math.max(0, offsetXPercent);
+ offsetXPercent = Math.min(100, offsetXPercent);
+
+ var percentageString = offsetXPercent + '%';
+ $seekBar.find('.fill').width(percentageString);
+ $seekBar.find('.thumb').css({left: percentageString});
+}
+
+var setupSeekBars = function() {
+
+  $seekBars = $('.player-bar .seek-bar');
+  $seekBars.click(function(event) {
+   updateSeekPercentage($(this), event);
+  });
+
+  $seekBars.find('.thumb').mousedown(function(event){
+    var $seekBar = $(this).parent();
+
+    $seekBar.addClass('no-animate');
+
+    $(document).bind('mousemove.thumb', function(event){
+      updateSeekPercentage($seekBar, event);
+    });
+
+    //cleanup
+    $(document).bind('mouseup.thumb', function(){
+      $seekBar.removeClass('no-animate');
+      
+      $(document).unbind('mousemove.thumb');
+      $(document).unbind('mouseup.thumb');
+    });
+
+  });
+
+};
+
+
+
+  // This 'if' condition is used to prevent the jQuery modifications
+  // from happening on non-Album view pages.
+  //  - Use a regex to validate that the url has "/album" in its path.
+  if (document.URL.match(/\/album.html/)) {
+  // Wait until the HTML is fully processed.
   $(document).ready(function() {
     changeAlbumView(albumPicasso);
- });
+    setupSeekBars();
+  });
 }
 
 

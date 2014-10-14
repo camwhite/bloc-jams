2671ddb45d2c93ec1f0f6f739ec71a49917bab73
Cameron White <camw85@mac.com>
2014-10-14 17:34:07 +0000

    Converted to single page app

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -272,7 +272,19 @@ var setupSeekBars = function() {
 // require("./profile");
 
 
-angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
+blocJams = angular.module('BlocJams', ['ui.router']);
+
+blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+ $locationProvider.html5Mode(true);
+
+ $stateProvider.state('landing', {
+   url: '/',
+   controller: 'Landing.controller',
+   templateUrl: '/templates/landing.html'
+ });
+}]);
+
+blocJams.controller('Landing.controller', ['$scope', function($scope) {
   $scope.subText = "Turn the music up!";
 
   $scope.subTextClicked = function() {

27523a50d49148c51cab7aac639636fe0cf90b32
Cameron White <camw85@mac.com>
2014-10-20 21:23:27 +0000

    Functional next and previous buttons

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -383,6 +383,10 @@ blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($s
 }]);
 
 blocJams.service('SongPlayer', function() {
+  var trackIndex = function(album, song) {
+   return album.songs.indexOf(song);
+  };
+
  return {
    currentSong: null,
    currentAlbum: null,
@@ -394,6 +398,24 @@ blocJams.service('SongPlayer', function() {
    pause: function() {
      this.playing = false;
    },
+   next: function() {
+     var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
+     currentTrackIndex++;
+     if (currentTrackIndex >= this.currentAlbum.songs.length) {
+       currentTrackIndex = 0;
+     }
+     
+     this.currentSong = this.currentAlbum.songs[currentTrackIndex];
+    },
+    previous: function() {
+     var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
+     currentTrackIndex--;
+     if (currentTrackIndex < 0) {
+       currentTrackIndex = this.currentAlbum.songs.length - 1;
+     }
+
+     this.currentSong = this.currentAlbum.songs[currentTrackIndex];
+    },
    setSong: function(album, song) {
      this.currentAlbum = album;
      this.currentSong = song;
